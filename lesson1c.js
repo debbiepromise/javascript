ddfca69b33c89e3787b84c152e3eeef570273851
debbiepromise <debbiebubbly@gmail.com>
2026-03-02 14:52:04 +0300

    Create lesson1c.js

---
+++ lesson1c.js
@@ -0,0 +1,8 @@
+// using let declare variables using let to storw height,temperature,points
+let height=38.5;
+let temperature=12;
+let points=50;
+// using console.log to print the variables
+console.log(height)
+console.log(temperature)
+console.log(points)
