6e904ffe562f6325a52fe44850bd508f7e5e1fbc
debbiepromise <debbiebubbly@gmail.com>
2026-03-02 14:50:16 +0300

    Create lesson1b.js

---
+++ lesson1b.js
@@ -0,0 +1,9 @@
+// variables= a variable is a container which is used for data types
+let username='debbie';
+const age = 19
+var height=34.7 
+// it is outdated
+// log all the three variables
+console.log(username)
+console.log(age)
+console.log(height)
