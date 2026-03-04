4f1b1c76b14d9313d856aa84963fb92280b073c0
debbiepromise <debbiebubbly@gmail.com>
2026-03-04 13:56:43 +0300

    Create lesson1d.js

---
+++ lesson1d.js
@@ -0,0 +1,23 @@
+// java string=a string is a collection of characters found inside quotes,either single or double 
+let username='ahadi'
+console.log(username);
+
+// js numbers=are integers and float
+let age = 19;
+console.log(age)
+let pi = 3.142
+// this is a floating point number
+
+// boolean=true or false
+let isadult = false;
+let isregistered = true;
+console.log(isadult)
+console.log(isregistered)
+
+// undefined
+let somevariables
+console.log(somevariables)
+
+// null
+let user=null
+console.log(user)
