1f7e5794465ab7e29ffd913af6d17efb929b2b51
debbiepromise <debbiebubbly@gmail.com>
2026-03-04 13:57:37 +0300

    Create lesson1e.js

---
+++ lesson1e.js
@@ -0,0 +1,16 @@
+// objects and array
+// an object is a collection of key value pairs where each key(also called a property) is associated with a value.the value can be any valid javascript data type-
+let person={
+    name:'ahadi',
+    age:19,
+    isadmin:true
+
+}
+// log
+console.log(person);
+
+// an array is a special type of object used to store a list of values in an ordered manner.they are indexed by numbers starting from 0
+// array of fruits and console.log
+let fruits=['apple','orange','grapes','pineapples'];
+console.log(fruits);
+console.log(fruits[3])
